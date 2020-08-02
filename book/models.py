53561730cbfb0882409fed3c9a60dfe2210f826f
MilenaVujicic <vujicic_milena@uns.ac.rs>
2020-08-02 22:10:35 +0200

    Added basic model

--- book/models.py
+++ book/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from publisher.models import Publisher
+from author.models import  Author
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(blank=False, null=False, max_length=512)
+    pages = models.PositiveSmallIntegerField(default=1)
+    description = models.TextField()
+    isbn = models.CharField(max_length=13)
+    status = models.TextChoices("READ", "TO BE READ")
+    lent = models.BooleanField(default=False)
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
+    authors = models.ManyToManyField(Author)
--- publisher/models.py
+++ publisher/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+
+class Publisher(models.Model):
+    name = models.CharField(null=False, blank=False, max_length=512)
