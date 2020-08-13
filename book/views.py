d5218154b7068a0622f37e13bc6788742aaef484
MilenaVujicic <vujicic_milena@uns.ac.rs>
2020-08-13 21:39:34 +0200

    Added crud operations for book, review and publisher

--- book/views.py
+++ book/views.py
@@ -4,9 +4,11 @@ from django.http import HttpResponse
 from django.http import JsonResponse
 from .models import Book
 from .serializers import BookSerializerBasic
+from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
 
 
+@csrf_exempt
 def book_list(request):
     if request.method == 'GET':
         books = Book.objects.all()
@@ -18,3 +20,26 @@ def book_list(request):
         if serializer.is_valid():
             serializer.save()
             return JsonResponse(serializer.data, status=200)
+
+
+@csrf_exempt
+def book_entity(request, id):
+    try:
+        book = Book.objects.get(id=id)
+    except Book.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'DELETE':
+        book.delete()
+        return HttpResponse(status=200)
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = BookSerializerBasic(book, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=200)
+        return JsonResponse(serializer.errors, status=400)
+    elif request.method == 'GET':
+        serializer = BookSerializerBasic(book)
+        return JsonResponse(serializer.data, status=200)
+
--- review/views.py
+++ review/views.py
@@ -4,10 +4,11 @@ from django.http import HttpResponse
 from django.http import JsonResponse
 from .models import Review
 from .serializers import ReviewSerializer
-
+from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
 
 
+@csrf_exempt
 def review_list(request):
     if request.method == 'GET':
         reviews = Review.objects.all()
@@ -19,3 +20,24 @@ def review_list(request):
         if serializer.is_valid():
             serializer.save()
             return JsonResponse(serializer.data, status=200)
+
+
+def review_entity(request, id):
+    try:
+        review = Review.objects.get(id=id)
+    except Review.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'DELETE':
+        review.delete()
+        return HttpResponse(status=200)
+    elif request.method == 'PUT':
+        data = JSONParser.parse(request)
+        serializer = ReviewSerializer(review, data=data)
+        if serializer.is_valid():
+            return JsonResponse(serializer.data, status=200)
+        return JsonResponse(serializer.errors, status=400)
+    elif request.method == 'GET':
+        serializer = ReviewSerializer(review)
+        return JsonResponse(serializer.data, status=200)
+
